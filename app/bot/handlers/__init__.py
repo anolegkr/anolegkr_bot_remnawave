0641171afba54f3538df6fe02f533d220e058277
An Oleg <anolegkr@gmail.com>
2026-02-24 20:31:06 +0300

    Update __init__.py

--- app/bot/handlers/__init__.py
+++ app/bot/handlers/__init__.py
@@ -1,3 +1,13 @@
-from . import start, menu, subscribe, donate, admin
+from .start import router as start_router
+from .menu import router as menu_router
+from .subscribe import router as subscribe_router
+from .donate import router as donate_router
+from .admin import router as admin_router
 
-__all__ = ["start", "menu", "subscribe", "donate", "admin"]
+__all__ = [
+    "start_router",
+    "menu_router",
+    "subscribe_router",
+    "donate_router",
+    "admin_router",
+]
