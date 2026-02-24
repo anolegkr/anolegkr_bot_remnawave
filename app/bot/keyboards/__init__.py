8bc36918c9ee6da3745eec414df0f9eb457348a0
An Oleg <anolegkr@gmail.com>
2026-02-24 20:30:20 +0300

    Update __init__.py

--- app/bot/keyboards/__init__.py
+++ app/bot/keyboards/__init__.py
@@ -1,1 +1,9 @@
 # Services package initializer
+from .main_menu import get_main_menu_keyboard, get_admin_keyboard
+from .donate import get_donate_keyboard
+
+__all__ = [
+    "get_main_menu_keyboard",
+    "get_admin_keyboard",
+    "get_donate_keyboard",
+]
