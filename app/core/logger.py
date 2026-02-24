44287363e0b194175e57a4e011ee01039e588d01
An Oleg <anolegkr@gmail.com>
2026-02-24 20:28:41 +0300

    Create logger.py

---
+++ app/core/logger.py
@@ -0,0 +1,14 @@
+import logging
+import sys
+from app.core.config import settings
+
+
+def setup_logger():
+    logging.basicConfig(
+        level=getattr(logging, settings.LOG_LEVEL.upper()),
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        handlers=[
+            logging.StreamHandler(sys.stdout)
+        ]
+    )
+    return logging.getLogger("anolegkr_bot")
